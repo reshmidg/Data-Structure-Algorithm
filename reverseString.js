c855fe5b971d0d1e9e23022cc5381acbb9fbaa96
reshmi <reshmi.datta35@gmail.com>
2018-11-18 01:02:53 +0530

    Reverse String

--- reverseString.js
+++ reverseString.js
@@ -10,3 +10,40 @@ function reverse(str){
 var reverseString = reverse('apple');
 
 console.log(reverseString);
+
+//Solution 2
+function reverse(str){
+	return str.split('').reverse().join();
+}
+
+//Other solution of for loop
+function reverse(str){
+	let reversedString = '';
+	
+	for(let character of str){
+		reversedString = character + reversedString;
+	}
+	return reversedString;
+}
+
+var reverseString = reverse('apple');
+
+console.log(reverseString);
+
+// Solution 3
+function reverse(str){
+	let reversed = '';
+	return str.split('').reduce((reversed,character)=>{
+		return character+reversed;
+	},'');
+}
+var reversedStr = reverse('apple');
+console.log(reversedStr);
+
+// Optimize solution 3
+function reverse(str){
+	let rev = '';
+	return str.split('').reduce((rev,ch)=>ch+rev,'');
+}
+var reversedStr = reverse('apple');
+console.log(reversedStr);
