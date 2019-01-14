cd19c4158233153408ab895a01124bf35082a9bc
reshmi <reshmi.datta35@gmail.com>
2019-01-15 00:04:16 +0530

    Armstrong number

--- Mathematical_problems/armstrong_number.js
+++ Mathematical_problems/armstrong_number.js
@@ -1,3 +1,5 @@
+/* An Armstrong number of three digits is an integer such that the sum of the cubes of its digits is equal to the number itself. 
+For example, 371 is an Armstrong number since 33 + 73 + 13 = 371*/
 //3 digit Armstrong number
 function armStrong(num){
 let sumOfAllDigits = 0,originalNumber = num;
