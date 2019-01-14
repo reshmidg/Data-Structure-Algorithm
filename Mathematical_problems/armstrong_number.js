5875f0e17bb9a5a57a79106d7d420bc85022df06
reshmi <reshmi.datta35@gmail.com>
2019-01-14 23:58:42 +0530

    Armstrong number

---
+++ Mathematical_problems/armstrong_number.js
@@ -0,0 +1,35 @@
+//3 digit Armstrong number
+function armStrong(num){
+let sumOfAllDigits = 0,originalNumber = num;
+	while(num>0){
+        const unitDigit = num%10;
+		sumOfAllDigits += (unitDigit * unitDigit * unitDigit);
+        const tensDigit = Math.floor(num/10);
+		num = Math.floor(num/10);
+    }
+	if(sumOfAllDigits === originalNumber){
+		return "Number is an Armstrong number";
+    }
+	else{
+		return "Number is not an Armstrong number";
+	}
+}
+console.log(armStrong(200));
+
+
+//any digit armstrong number
+function anyDigitArmstrongNumber(num){
+	const originalNumber = num;
+	let numberArray = num.toString().split('');
+	let numberOfDigits = numberArray.length;
+	let sumOfDigits = 0;
+	numberArray.forEach((digit) => {
+		sumOfDigits += Math.pow(parseInt(digit),numberOfDigits);
+	});
+	if(sumOfDigits === originalNumber){
+		console.log('Armstrong number');
+	}else{
+		console.log('Not an Armstrong number');
+	}
+}
+anyDigitArmstrongNumber(1634);
