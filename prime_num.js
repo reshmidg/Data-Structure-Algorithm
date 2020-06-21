16c3fdee2a407d81e97ee430bdf93ebd7b162441
Reshmi <v-rdattagupta@expedia.com>
2020-06-21 12:15:58 +0530

    Find out whether a number is prime number or not

--- prime_num.js
+++ prime_num.js
@@ -1,6 +1,4 @@
-// If a number n is not a prime, it can be factored into two factors a and b:
-// 
-// n = a * b
+// If a number n is not a prime, it can be factored into two factors a and b: n = a * b
 // Now a and b can't be both greater than the square root of n, 
 // since then the product a * b would be greater than sqrt(n) * sqrt(n) = n. 
 // So in any factorization of n, at least one of the factors must be smaller than the square root of n, 
