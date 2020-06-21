5fc9ddcf9986809a21490bc8fa4d0ca36f9cfd9c
Reshmi <v-rdattagupta@expedia.com>
2020-06-21 12:14:57 +0530

    Find out whether a number is prime number or not

--- prime_num.js
+++ prime_num.js
@@ -1,23 +1,21 @@
+// If a number n is not a prime, it can be factored into two factors a and b:
+// 
+// n = a * b
+// Now a and b can't be both greater than the square root of n, 
+// since then the product a * b would be greater than sqrt(n) * sqrt(n) = n. 
+// So in any factorization of n, at least one of the factors must be smaller than the square root of n, 
+// and if we can't find any factors less than or equal to the square root, n must be a prime.
+
 function isPrime(num){
-	var isPrime = false;
 	if(num === 1){
-		isPrime = false;
-		return isPrime;
-	}else if(num === 2){
-		isPrime = true;
-		return isPrime;
-	}else{
-	    for(let i=2;i<=Math.sqrt(num);i++){
-	        if(num % i === 0){
-	            isPrime = false;
-				return isPrime;
-	        }else{
-	            isPrime = true;
-				
-	        }
+		return 'Number is not prime';
+	}
+	for(let i=2;i<=Math.sqrt(num);i++){
+	    if(num % i === 0){
+	        return 'Number is prime';
 	    }
-	    return isPrime;
-    }
+	}
+	return 'Number is not prime';
 }
 
 console.log(isPrime(121));
